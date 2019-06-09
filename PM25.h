54e1ebbb5943c2c51985538828a073efb4e382e0
Aidan Goettsch <aidangoettsch@gmail.com>
2019-06-09 15:43:54 -0400

    more sensor code

---
+++ PM25.h
@@ -0,0 +1,26 @@
+#ifndef PM25_h
+#define PM25_h
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+
+struct pms5003data {
+    public:
+        uint16_t framelen;
+        uint16_t pm10_standard, pm25_standard, pm100_standard;
+        uint16_t pm10_env, pm25_env, pm100_env;
+        uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
+        uint16_t unused;
+        uint16_t checksum;
+};
+
+class PM25 {
+    public:
+        PM25(int rxPin, int txPin);
+        boolean readData();
+        pms5003data data;
+    private:
+        SoftwareSerial _pm25Serial;
+};
+
+#endif
