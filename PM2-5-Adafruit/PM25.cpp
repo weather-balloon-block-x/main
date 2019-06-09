1fa551062ec4d30e16f061f5c8dcdd1d6ac28be9
Aidan Goettsch <aidangoettsch@gmail.com>
2019-06-09 10:49:44 -0400

    pm25

---
+++ PM2-5-Adafruit/PM25.cpp
@@ -0,0 +1,60 @@
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "PM25.h"
+
+PM25::PM25(int rxPin, txPin) {
+    _pm25Serial = new SoftwareSerial(rxPin, txPIn);
+    _pm25Serial.begin(9600)
+}
+
+*ms5003data PM25:readData() {
+    struct pms5003data data;
+
+    if (! _pm25Serial->available()) {
+        return false;
+      }
+      
+      // Read a byte at a time until we get to the special '0x42' start-byte
+      if (_pm25Serial->peek() != 0x42) {
+        _pm25Serial->read();
+        return false;
+      }
+    
+      // Now read all 32 bytes
+      if (_pm25Serial->available() < 32) {
+        return false;
+      }
+        
+      uint8_t buffer[32];    
+      uint16_t sum = 0;
+      _pm25Serial->readBytes(buffer, 32);
+    
+      // get checksum ready
+      for (uint8_t i=0; i<30; i++) {
+        sum += buffer[i];
+      }
+    
+      /* debugging
+      for (uint8_t i=2; i<32; i++) {
+        Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
+      }
+      Serial.println();
+      */
+      
+      // The data comes in endian'd, this solves it so it works on all platforms
+      uint16_t buffer_u16[15];
+      for (uint8_t i=0; i<15; i++) {
+        buffer_u16[i] = buffer[2 + i*2 + 1];
+        buffer_u16[i] += (buffer[2 + i*2] << 8);
+      }
+    
+      // put it into a nice struct :)
+      memcpy((void *)&data, (void *)buffer_u16, 30);
+    
+      if (sum != data.checksum) {
+        Serial.println("Checksum failure");
+        return false;
+      }
+      // success!
+      return &data;
+}
