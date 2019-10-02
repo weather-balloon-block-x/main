d141dc68bdfc051ab419d3653efb9504bd4a8cab
Max C <dumblole@gmail.com>
2019-10-01 22:29:00 -0400

    make sure to add comments
    make sure to add comments so the weather balloon doesn't explode

--- src/DS18B20/DS18B20.h
+++ src/DS18B20/DS18B20.h
@@ -1,6 +1,7 @@
 #ifndef DS18B20_h
 #define DS18B20_h
 
+// include libraries
 #include "Arduino.h"
 #include <Wire.h>
 #include <DallasTemperature.h>
@@ -8,9 +9,11 @@
 
 class DS18B20 {
     public:
+        //public code
         DS18B20(int oneWirePort);
         float getTemp();
     private:
+        //private code
         OneWire _wire;
         DallasTemperature _sensor;
 };
